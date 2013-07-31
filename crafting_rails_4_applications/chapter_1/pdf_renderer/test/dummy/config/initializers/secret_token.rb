5d6334bd3478261dca98f45c40f90152b5f5b890
Bruno Henrique - Garu <squall.bruno@gmail.com>
2013-07-31 14:07:59 -0300

    init chapter1

---
+++ crafting_rails_4_applications/chapter_1/pdf_renderer/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '0850b759d39a4f3c43cee18ac122bfbdd784706279572ea5f7fac1a209701510ff14a5fdd004a745a8a8071d5ff858f78769224034d8657475d5e5319e92bf92'
