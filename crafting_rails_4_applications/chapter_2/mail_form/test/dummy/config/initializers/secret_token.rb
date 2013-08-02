5016bb3605a2b3690cccd8513905613a9e02fd95
Bruno Henrique - Garu <squall.bruno@gmail.com>
2013-08-02 12:50:52 -0300

    init chapter 2

---
+++ crafting_rails_4_applications/chapter_2/mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '8b2e906194d172d41092a5e0092e09760ec58e512234f8b00c853823ad26eb976cb867f56785cfcfd163f741591ba14d8c9182c3b8773d07ecfe1a9ba6a5cd2c'
