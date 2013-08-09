f1e1aefd9d469698e8f3fc5ca0b1b6836815c68b
Bruno Henrique - Garu <squall.bruno@gmail.com>
2013-08-09 15:32:31 -0300

    New behavior

--- crafting_rails_4_applications/chapter_2/mail_form/lib/mail_form/base.rb
+++ crafting_rails_4_applications/chapter_2/mail_form/lib/mail_form/base.rb
@@ -1,12 +1,25 @@
 module MailForm
   class Base
     include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+
     attribute_method_prefix 'clear_'
     attribute_method_suffix '?'
 
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
+    end
+
+    def to_model
+      self
+    end
+
+    def persisted?
+      false
     end
 
     protected
