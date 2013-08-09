a3307f816e5215f7ccd48ac59160c7f373748aa4
Bruno Henrique - Garu <squall.bruno@gmail.com>
2013-08-09 17:39:02 -0300

    Update code

--- crafting_rails_4_applications/chapter_2/mail_form/lib/mail_form/base.rb
+++ crafting_rails_4_applications/chapter_2/mail_form/lib/mail_form/base.rb
@@ -6,12 +6,24 @@ module MailForm
     extend ActiveModel::Naming
     extend ActiveModel::Translation
 
+    class_attribute :attribute_names
+    self.attribute_names = []
+
     attribute_method_prefix 'clear_'
     attribute_method_suffix '?'
 
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
+      self.attribute_names += names
+    end
+
+    def deliver
+      if valid?
+        MailForm::Notifier.contact(self).deliver
+      else
+        false
+      end
     end
 
     def to_model
